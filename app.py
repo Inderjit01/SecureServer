2b66d4127197c737d474798bdbfd8ae651459a3f
Inderjit01 <Inderjitsingh0817@gmail.com>
2026-01-31 13:57:51 -0800

    setting up environemnt

---
+++ app.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 31 11:47:16 2026
+
+@author: inder
+"""
+
---
+++ models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 31 13:53:33 2026
+
+@author: inder
+"""
+
